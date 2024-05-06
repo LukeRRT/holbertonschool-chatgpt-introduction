bbdb14f36cd83efdddbe1e68b2a392875b163b6e
LukeRRT <aumrungruj.t@gmail.com>
2024-05-06 05:45:49 +0000

    First Review

---
+++ debugging/factorial_recursive.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    """
+    Calculate the factorial of a given number using recursion.
+
+    Parameters:
+        n (int): The number whose factorial is to be calculated.
+
+    Returns:
+        int: The factorial of the given number.
+    """
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+f = factorial(int(sys.argv[1]))
+print(f)
